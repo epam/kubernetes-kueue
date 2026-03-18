kubernetes-sigs/kueue

Add CohortBorrowingLimit metric

pkg/cache/scheduler/cohort_metrics.go
@@ -18,6 +18,7 @@ package scheduler
 
 import (
 	"github.com/go-logr/logr"
+	"k8s.io/utils/ptr"
 
 	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta2"
 	"sigs.k8s.io/kueue/pkg/cache/hierarchy"
@@ -26,9 +27,10 @@ import (
 )
 
 type cohortMetricPoint struct {
-	cohortName     kueue.CohortReference
-	flavorResource resources.FlavorResource
-	qty            int64
+	cohortName          kueue.CohortReference
+	flavorResource      resources.FlavorResource
+	subtreeNominalQuota int64
+	borrowingLimit      *int64
 }
 
 func (c *Cache) RecordCohortMetrics(log logr.Logger, cohortName kueue.CohortReference) {
@@ -72,23 +74,34 @@ func (c *Cache) collectCohortMetricPoints(cohortName kueue.CohortReference, simu
 		return nil
 	}
 
-	removedQuota := ch.resourceNode.SubtreeQuota
+	removedSubtreeQuota := ch.resourceNode.SubtreeQuota
+	removedQuotas := ch.resourceNode.Quotas
 	var points []cohortMetricPoint
 	for ancestor := range ch.PathSelfToRoot() {
-		quotas := ancestor.resourceNode.SubtreeQuota
+		ancestorSubtreeQuota := ancestor.resourceNode.SubtreeQuota
+
 		if simulateRemoval {
-			quotas = removedQuota
+			ancestorSubtreeQuota = removedSubtreeQuota
 		}
 
-		for flr, qty := range quotas {
+		isRemovedCohort := simulateRemoval && ancestor.Name == cohortName
+
+		for flr, qty := range ancestorSubtreeQuota {
 			if simulateRemoval {
 				qty = max(ancestor.resourceNode.SubtreeQuota[flr]-qty, 0)
 			}
-			points = append(points, cohortMetricPoint{
-				cohortName:     ancestor.Name,
-				flavorResource: flr,
-				qty:            qty,
-			})
+			p := cohortMetricPoint{
+				cohortName:          ancestor.Name,
+				flavorResource:      flr,
+				subtreeNominalQuota: qty,
+				// Default behavior: borrowing limit is local to the current ancestor.
+				borrowingLimit: ancestor.resourceNode.Quotas[flr].BorrowingLimit,
+			}
+
+			if isRemovedCohort && removedQuotas[flr].BorrowingLimit != nil {
+				p.borrowingLimit = ptr.To(int64(0))
+			}
+			points = append(points, p)
 		}
 	}
 	return points
@@ -99,19 +112,35 @@ func (c *Cache) withCohortLogger(log logr.Logger, cohortName kueue.CohortReferen
 }
 
 func (c *Cache) applyCohortMetricPoint(p cohortMetricPoint) {
-	if p.qty <= 0 {
+	if p.subtreeNominalQuota > 0 {
+		metrics.ReportCohortSubtreeQuota(
+			p.cohortName,
+			p.flavorResource.Flavor,
+			p.flavorResource.Resource,
+			p.subtreeNominalQuota,
+			c.roleTracker,
+		)
+	} else {
 		metrics.ClearCohortSubtreeQuota(
 			p.cohortName,
-			string(p.flavorResource.Flavor),
-			string(p.flavorResource.Resource),
+			p.flavorResource.Flavor,
+			p.flavorResource.Resource,
 		)
-		return
 	}
-	metrics.ReportCohortSubtreeQuota(
-		p.cohortName,
-		string(p.flavorResource.Flavor),
-		string(p.flavorResource.Resource),
-		float64(p.qty),
-		c.roleTracker,
-	)
+
+	if p.borrowingLimit != nil && *p.borrowingLimit > 0 {
+		metrics.ReportCohortSubtreeBorrowingLimits(
+			p.cohortName,
+			p.flavorResource.Flavor,
+			p.flavorResource.Resource,
+			*p.borrowingLimit,
+			c.roleTracker,
+		)
+	} else {
+		metrics.ClearCohortSubtreeBorrowingLimits(
+			p.cohortName,
+			p.flavorResource.Flavor,
+			p.flavorResource.Resource,
+		)
+	}
 }

pkg/metrics/metrics.go
@@ -20,6 +20,7 @@ import (
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/metrics"
 
@@ -254,6 +255,10 @@ var (
 	// +metricsdoc:group=cohort
 	// +metricsdoc:labels=cohort="the name of the Cohort",flavor="the resource flavor name",resource="the resource name",replica_role="one of `leader`, `follower`, or `standalone`"
 	CohortSubtreeQuota *prometheus.GaugeVec
+
+	// +metricsdoc:group=cohort
+	// +metricsdoc:labels=cohort="the name of the Cohort",flavor="the resource flavor name",resource="the resource name",replica_role="one of `leader`, `follower`, or `standalone`"
+	CohortSubtreeBorrowingLimit *prometheus.GaugeVec
 )
 
 func trackGaugeVec(g *prometheus.GaugeVec) *prometheus.GaugeVec {
@@ -726,6 +731,17 @@ If the Cohort has a weight of zero and is borrowing, this will return NaN.`,
 			Help:      `Reports the cohort's nominal quota aggregated within the cohort's subtree. The values are reported per resource and flavor`,
 		}, []string{"cohort", "flavor", "resource", "replica_role"},
 	))
+	// +metricsdoc:group=cohort
+	// +metricsdoc:labels=cohort="the name of the Cohort",flavor="the resource flavor name",resource="the resource name",replica_role="one of `leader`, `follower`, or `standalone`"
+	CohortSubtreeBorrowingLimit = trackGaugeVec(prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Subsystem: constants.KueueName,
+			Name:      "cohort_subtree_borrowing_limit",
+			Help: `Reports the maximum amount of resources that the Cohort subtree 
+(its child Cohorts and ClusterQueues) can borrow from its parent subtree.
+The values are reported per resource and flavor`,
+		}, []string{"cohort", "flavor", "resource", "replica_role"},
+	))
 }
 
 func init() {
@@ -893,6 +909,7 @@ func ClearLocalQueueMetrics(lq LocalQueueReference) {
 }
 
 func ClearCohortMetrics(cohortName string) {
+	CohortSubtreeBorrowingLimit.DeletePartialMatch(prometheus.Labels{"cohort": cohortName})
 	CohortSubtreeQuota.DeletePartialMatch(prometheus.Labels{"cohort": cohortName})
 	CohortWeightedShare.DeletePartialMatch(prometheus.Labels{"cohort": cohortName})
 }
@@ -940,21 +957,38 @@ func ReportClusterQueueQuotas(cohort kueue.CohortReference, queue, flavor, resou
 	ClusterQueueResourceLendingLimit.WithLabelValues(string(cohort), queue, flavor, resource, role).Set(lending)
 }
 
-func ReportCohortSubtreeQuota(cohort kueue.CohortReference, flavor, resource string, quota float64, tracker *roletracker.RoleTracker) {
-	CohortSubtreeQuota.WithLabelValues(string(cohort), flavor, resource, roletracker.GetRole(tracker)).Set(quota)
+func ReportCohortSubtreeQuota(cohort kueue.CohortReference, flavor kueue.ResourceFlavorReference, resource corev1.ResourceName, quota int64, tracker *roletracker.RoleTracker) {
+	CohortSubtreeQuota.WithLabelValues(string(cohort), string(flavor), string(resource), roletracker.GetRole(tracker)).Set(float64(quota))
 }
 
-func ClearCohortSubtreeQuota(cohort kueue.CohortReference, flavor, resource string) {
+func ClearCohortSubtreeQuota(cohort kueue.CohortReference, flavor kueue.ResourceFlavorReference, resource corev1.ResourceName) {
 	lbls := prometheus.Labels{
 		"cohort": string(cohort),
 	}
 	if len(flavor) != 0 {
-		lbls["flavor"] = flavor
+		lbls["flavor"] = string(flavor)
 	}
 	if len(resource) != 0 {
-		lbls["resource"] = resource
+		lbls["resource"] = string(resource)
 	}
 	CohortSubtreeQuota.DeletePartialMatch(lbls)
+}
+
+func ReportCohortSubtreeBorrowingLimits(cohort kueue.CohortReference, flavor kueue.ResourceFlavorReference, resource corev1.ResourceName, borrowing int64, tracker *roletracker.RoleTracker) {
+	CohortSubtreeBorrowingLimit.WithLabelValues(string(cohort), string(flavor), string(resource), roletracker.GetRole(tracker)).Set(float64(borrowing))
+}
+
+func ClearCohortSubtreeBorrowingLimits(cohort kueue.CohortReference, flavor kueue.ResourceFlavorReference, resource corev1.ResourceName) {
+	lbls := prometheus.Labels{
+		"cohort": string(cohort),
+	}
+	if len(flavor) != 0 {
+		lbls["flavor"] = string(flavor)
+	}
+	if len(resource) != 0 {
+		lbls["resource"] = string(resource)
+	}
+	CohortSubtreeBorrowingLimit.DeletePartialMatch(lbls)
 }
 
 func ReportClusterQueueResourceReservations(cohort kueue.CohortReference, queue, flavor, resource string, usage float64, tracker *roletracker.RoleTracker) {
@@ -1084,6 +1118,7 @@ func Register() {
 		ClusterQueueResourceLendingLimit,
 		ClusterQueueWeightedShare,
 		CohortWeightedShare,
+		CohortSubtreeBorrowingLimit,
 		CohortSubtreeQuota,
 	)
 	if features.Enabled(features.LocalQueueMetrics) {

epam/kubernetes-kueue

pkg/cache/scheduler/cohort_metrics.go
@@ -18,6 +18,7 @@ package scheduler
 
 import (
 	"github.com/go-logr/logr"
+	"k8s.io/utils/ptr"
 
 	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta2"
 	"sigs.k8s.io/kueue/pkg/cache/hierarchy"
@@ -26,9 +27,10 @@ import (
 )
 
 type cohortMetricPoint struct {
-	cohortName     kueue.CohortReference
-	flavorResource resources.FlavorResource
-	qty            int64
+	cohortName          kueue.CohortReference
+	flavorResource      resources.FlavorResource
+	subtreeNominalQuota int64
+	borrowingLimit      *int64
 }
 
 func (c *Cache) RecordCohortMetrics(log logr.Logger, cohortName kueue.CohortReference) {
@@ -72,23 +74,34 @@ func (c *Cache) collectCohortMetricPoints(cohortName kueue.CohortReference, simu
 		return nil
 	}
 
-	removedQuota := ch.resourceNode.SubtreeQuota
+	removedSubtreeQuota := ch.resourceNode.SubtreeQuota
+	removedQuotas := ch.resourceNode.Quotas
 	var points []cohortMetricPoint
 	for ancestor := range ch.PathSelfToRoot() {
-		quotas := ancestor.resourceNode.SubtreeQuota
+		ancestorSubtreeQuota := ancestor.resourceNode.SubtreeQuota
+
 		if simulateRemoval {
-			quotas = removedQuota
+			ancestorSubtreeQuota = removedSubtreeQuota
 		}
 
-		for flr, qty := range quotas {
+		isRemovedCohort := simulateRemoval && ancestor.Name == cohortName
+
+		for flr, qty := range ancestorSubtreeQuota {
 			if simulateRemoval {
 				qty = max(ancestor.resourceNode.SubtreeQuota[flr]-qty, 0)
 			}
-			points = append(points, cohortMetricPoint{
-				cohortName:     ancestor.Name,
-				flavorResource: flr,
-				qty:            qty,
-			})
+			p := cohortMetricPoint{
+				cohortName:          ancestor.Name,
+				flavorResource:      flr,
+				subtreeNominalQuota: qty,
+				// Default behavior: borrowing limit is local to the current ancestor.
+				borrowingLimit: ancestor.resourceNode.Quotas[flr].BorrowingLimit,
+			}
+
+			if isRemovedCohort && removedQuotas[flr].BorrowingLimit != nil {
+				p.borrowingLimit = ptr.To(int64(0))
+			}
+			points = append(points, p)
 		}
 	}
 	return points
@@ -99,19 +112,35 @@ func (c *Cache) withCohortLogger(log logr.Logger, cohortName kueue.CohortReferen
 }
 
 func (c *Cache) applyCohortMetricPoint(p cohortMetricPoint) {
-	if p.qty <= 0 {
+	if p.subtreeNominalQuota > 0 {
+		metrics.ReportCohortSubtreeQuota(
+			p.cohortName,
+			p.flavorResource.Flavor,
+			p.flavorResource.Resource,
+			p.subtreeNominalQuota,
+			c.roleTracker,
+		)
+	} else {
 		metrics.ClearCohortSubtreeQuota(
 			p.cohortName,
-			string(p.flavorResource.Flavor),
-			string(p.flavorResource.Resource),
+			p.flavorResource.Flavor,
+			p.flavorResource.Resource,
 		)
-		return
 	}
-	metrics.ReportCohortSubtreeQuota(
-		p.cohortName,
-		string(p.flavorResource.Flavor),
-		string(p.flavorResource.Resource),
-		float64(p.qty),
-		c.roleTracker,
-	)
+
+	if p.borrowingLimit != nil {
+		metrics.ReportCohortSubtreeBorrowingLimit(
+			p.cohortName,
+			p.flavorResource.Flavor,
+			p.flavorResource.Resource,
+			*p.borrowingLimit,
+			c.roleTracker,
+		)
+	} else {
+		metrics.ClearCohortSubtreeBorrowingLimit(
+			p.cohortName,
+			p.flavorResource.Flavor,
+			p.flavorResource.Resource,
+		)
+	}
 }

pkg/metrics/metrics.go
@@ -20,6 +20,7 @@ import (
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/metrics"
 
@@ -254,6 +255,10 @@ var (
 	// +metricsdoc:group=cohort
 	// +metricsdoc:labels=cohort="the name of the Cohort",flavor="the resource flavor name",resource="the resource name",replica_role="one of `leader`, `follower`, or `standalone`"
 	CohortSubtreeQuota *prometheus.GaugeVec
+
+	// +metricsdoc:group=cohort
+	// +metricsdoc:labels=cohort="the name of the Cohort",flavor="the resource flavor name",resource="the resource name",replica_role="one of `leader`, `follower`, or `standalone`"
+	CohortSubtreeBorrowingLimit *prometheus.GaugeVec
 )
 
 func trackGaugeVec(g *prometheus.GaugeVec) *prometheus.GaugeVec {
@@ -728,6 +733,16 @@ If the Cohort has a weight of zero and is borrowing, this will return NaN.`,
 			Help:      `Reports the cohort's nominal quota aggregated within the cohort's subtree. The values are reported per resource and flavor`,
 		}, []string{"cohort", "flavor", "resource", "replica_role"},
 	))
+
+	CohortSubtreeBorrowingLimit = trackGaugeVec(prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Subsystem: constants.KueueName,
+			Name:      "cohort_subtree_borrowing_limit",
+			Help: `Reports the maximum amount of resources that the Cohort subtree 
+(its child Cohorts and ClusterQueues) can borrow from its parent subtree.
+The values are reported per resource and flavor`,
+		}, []string{"cohort", "flavor", "resource", "replica_role"},
+	))
 }
 
 func init() {
@@ -914,6 +929,7 @@ func ClearLocalQueueMetrics(lq LocalQueueReference) {
 }
 
 func ClearCohortMetrics(cohortName string) {
+	CohortSubtreeBorrowingLimit.DeletePartialMatch(prometheus.Labels{"cohort": cohortName})
 	CohortSubtreeQuota.DeletePartialMatch(prometheus.Labels{"cohort": cohortName})
 	CohortWeightedShare.DeletePartialMatch(prometheus.Labels{"cohort": cohortName})
 }
@@ -965,21 +981,38 @@ func ReportClusterQueueQuotas(cohort kueue.CohortReference, queue, flavor, resou
 	ClusterQueueResourceLendingLimit.WithLabelValues(labels...).Set(lending)
 }
 
-func ReportCohortSubtreeQuota(cohort kueue.CohortReference, flavor, resource string, quota float64, tracker *roletracker.RoleTracker) {
-	CohortSubtreeQuota.WithLabelValues(string(cohort), flavor, resource, roletracker.GetRole(tracker)).Set(quota)
+func ReportCohortSubtreeQuota(cohort kueue.CohortReference, flavor kueue.ResourceFlavorReference, resource corev1.ResourceName, quota int64, tracker *roletracker.RoleTracker) {
+	CohortSubtreeQuota.WithLabelValues(string(cohort), string(flavor), string(resource), roletracker.GetRole(tracker)).Set(float64(quota))
 }
 
-func ClearCohortSubtreeQuota(cohort kueue.CohortReference, flavor, resource string) {
+func ClearCohortSubtreeQuota(cohort kueue.CohortReference, flavor kueue.ResourceFlavorReference, resource corev1.ResourceName) {
 	lbls := prometheus.Labels{
 		"cohort": string(cohort),
 	}
 	if len(flavor) != 0 {
-		lbls["flavor"] = flavor
+		lbls["flavor"] = string(flavor)
 	}
 	if len(resource) != 0 {
-		lbls["resource"] = resource
+		lbls["resource"] = string(resource)
 	}
 	CohortSubtreeQuota.DeletePartialMatch(lbls)
+}
+
+func ReportCohortSubtreeBorrowingLimit(cohort kueue.CohortReference, flavor kueue.ResourceFlavorReference, resource corev1.ResourceName, borrowing int64, tracker *roletracker.RoleTracker) {
+	CohortSubtreeBorrowingLimit.WithLabelValues(string(cohort), string(flavor), string(resource), roletracker.GetRole(tracker)).Set(float64(borrowing))
+}
+
+func ClearCohortSubtreeBorrowingLimit(cohort kueue.CohortReference, flavor kueue.ResourceFlavorReference, resource corev1.ResourceName) {
+	lbls := prometheus.Labels{
+		"cohort": string(cohort),
+	}
+	if len(flavor) != 0 {
+		lbls["flavor"] = string(flavor)
+	}
+	if len(resource) != 0 {
+		lbls["resource"] = string(resource)
+	}
+	CohortSubtreeBorrowingLimit.DeletePartialMatch(lbls)
 }
 
 func ReportClusterQueueResourceReservations(cohort kueue.CohortReference, queue, flavor, resource string, usage float64, customLabelValues []string, tracker *roletracker.RoleTracker) {
@@ -1145,6 +1178,7 @@ func Register() {
 		ClusterQueueResourceLendingLimit,
 		ClusterQueueWeightedShare,
 		CohortWeightedShare,
+		CohortSubtreeBorrowingLimit,
 		CohortSubtreeQuota,
 	)
 	if features.Enabled(features.LocalQueueMetrics) {