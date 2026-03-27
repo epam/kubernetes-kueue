kubernetes-sigs/kueue

featureGates unit tests cleanup
Added features.SetFeatureGatesDuringTest function

Removed outdated TODO

pkg/controller/jobs/statefulset/statefulset_webhook_test.go
@@ -29,6 +29,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/component-base/featuregate"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -183,10 +184,10 @@ func TestDefault(t *testing.T) {
 
 func TestValidateCreate(t *testing.T) {
 	testCases := map[string]struct {
-		sts              *appsv1.StatefulSet
-		wantErr          error
-		wantWarns        admission.Warnings
-		admissionGatedBy bool
+		sts          *appsv1.StatefulSet
+		wantErr      error
+		wantWarns    admission.Warnings
+		featureGates map[featuregate.Feature]bool
 	}{
 		"without queue": {
 			sts: testingstatefulset.MakeStatefulSet("test-pod", "").Obj(),
@@ -218,48 +219,48 @@ func TestValidateCreate(t *testing.T) {
 				Queue("queue").
 				Annotation(kueueconstants.AdmissionGatedByAnnotation, "example.com/controller").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: true,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"AdmissionGatedBy annotation - trailing space": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
 				Queue("queue").
 				Annotation(kueueconstants.AdmissionGatedByAnnotation, "example.com/gate ").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: true,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"AdmissionGatedBy annotation - space before comma": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
 				Queue("queue").
 				Annotation(kueueconstants.AdmissionGatedByAnnotation, "example.com/gate ,example.com/gate2").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: true,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"AdmissionGatedBy annotation - space after comma": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
 				Queue("queue").
 				Annotation(kueueconstants.AdmissionGatedByAnnotation, "example.com/gate, example.com/gate2").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: true,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"AdmissionGatedBy annotation - leading space": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
 				Queue("queue").
 				Annotation(kueueconstants.AdmissionGatedByAnnotation, " example.com/gate").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: true,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"AdmissionGatedBy annotation - multiple gates": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
 				Queue("queue").
 				Annotation(kueueconstants.AdmissionGatedByAnnotation, "example.com/a,not.example.com/b").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: true,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"invalid AdmissionGatedBy annotation - not in subdomain/path format": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
@@ -269,7 +270,7 @@ func TestValidateCreate(t *testing.T) {
 			wantErr: field.ErrorList{
 				field.Invalid(admissionGatedByAnnotationsPath, "this is an invalid value", "must be a domain-prefixed path (such as \"acme.io/foo\")"),
 			}.ToAggregate(),
-			admissionGatedBy: true,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"invalid AdmissionGatedBy annotation - duplicate gates": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
@@ -279,7 +280,7 @@ func TestValidateCreate(t *testing.T) {
 			wantErr: field.ErrorList{
 				field.Invalid(admissionGatedByAnnotationsPath, "duplicates.are/invalid,duplicates.are/invalid", "duplicate gate name: duplicates.are/invalid"),
 			}.ToAggregate(),
-			admissionGatedBy: true,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"invalid AdmissionGatedBy annotation - gate name too long": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
@@ -289,7 +290,7 @@ func TestValidateCreate(t *testing.T) {
 			wantErr: field.ErrorList{
 				field.TooLong(admissionGatedByAnnotationsPath, "", webhook.MaxGateNameLengthForAdmissionGatedBy),
 			}.ToAggregate(),
-			admissionGatedBy: true,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"invalid AdmissionGatedBy annotation - space in path component": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
@@ -299,7 +300,7 @@ func TestValidateCreate(t *testing.T) {
 			wantErr: field.ErrorList{
 				field.Invalid(admissionGatedByAnnotationsPath, "gate name", testutil.InvalidPathMessage),
 			}.ToAggregate(),
-			admissionGatedBy: true,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"invalid AdmissionGatedBy annotation - space in domain component": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
@@ -309,7 +310,7 @@ func TestValidateCreate(t *testing.T) {
 			wantErr: field.ErrorList{
 				field.Invalid(admissionGatedByAnnotationsPath, "example .com", testutil.InvalidRFC1123Message),
 			}.ToAggregate(),
-			admissionGatedBy: true,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"invalid AdmissionGatedBy annotation - multiple gates with one containing space": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
@@ -319,37 +320,37 @@ func TestValidateCreate(t *testing.T) {
 			wantErr: field.ErrorList{
 				field.Invalid(admissionGatedByAnnotationsPath, "invalid gate.com", testutil.InvalidRFC1123Message),
 			}.ToAggregate(),
-			admissionGatedBy: true,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"AdmissionGatedBy annotation with feature gate disabled - valid value": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
 				Queue("queue").
 				Annotation(kueueconstants.AdmissionGatedByAnnotation, "example.com/gate").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: false,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: false},
 		},
 		"AdmissionGatedBy annotation with feature gate disabled - invalid value": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
 				Queue("queue").
 				Annotation(kueueconstants.AdmissionGatedByAnnotation, "this is an invalid value").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: false,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: false},
 		},
 		"AdmissionGatedBy annotation with feature gate enabled - empty string": {
 			sts: testingstatefulset.MakeStatefulSet("test-sts", "default").
 				Queue("queue").
 				Annotation(kueueconstants.AdmissionGatedByAnnotation, "").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: true,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 	}
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
-			features.SetFeatureGateDuringTest(t, features.AdmissionGatedBy, tc.admissionGatedBy)
+			features.SetFeatureGatesDuringTest(t, tc.featureGates)
 			t.Cleanup(jobframework.EnableIntegrationsForTest(t, "pod"))
 			builder := utiltesting.NewClientBuilder()
 			client := builder.Build()
@@ -368,12 +369,12 @@ func TestValidateCreate(t *testing.T) {
 
 func TestValidateUpdate(t *testing.T) {
 	testCases := map[string]struct {
-		integrations     []string
-		objs             []runtime.Object
-		oldObj           *appsv1.StatefulSet
-		newObj           *appsv1.StatefulSet
-		wantErr          error
-		admissionGatedBy bool
+		integrations []string
+		objs         []runtime.Object
+		oldObj       *appsv1.StatefulSet
+		newObj       *appsv1.StatefulSet
+		wantErr      error
+		featureGates map[featuregate.Feature]bool
 	}{
 		"no changes": {
 			oldObj: &appsv1.StatefulSet{
@@ -948,7 +949,7 @@ func TestValidateUpdate(t *testing.T) {
 			wantErr: field.ErrorList{
 				field.Forbidden(admissionGatedByAnnotationsPath, "cannot add admission gate after creation"),
 			}.ToAggregate(),
-			admissionGatedBy: true,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"allow removing AdmissionGatedBy annotation with single gate": {
 			oldObj: testingstatefulset.MakeStatefulSet("test-sts", "default").
@@ -958,8 +959,8 @@ func TestValidateUpdate(t *testing.T) {
 			newObj: testingstatefulset.MakeStatefulSet("test-sts", "default").
 				Queue("queue").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: true,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"allow removing AdmissionGatedBy annotation with multiple gates": {
 			oldObj: testingstatefulset.MakeStatefulSet("test-sts", "default").
@@ -969,8 +970,8 @@ func TestValidateUpdate(t *testing.T) {
 			newObj: testingstatefulset.MakeStatefulSet("test-sts", "default").
 				Queue("queue").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: true,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"allow removing one gate from AdmissionGatedBy annotation": {
 			oldObj: testingstatefulset.MakeStatefulSet("test-sts", "default").
@@ -981,8 +982,8 @@ func TestValidateUpdate(t *testing.T) {
 				Queue("queue").
 				Annotation(kueueconstants.AdmissionGatedByAnnotation, "example.com/controller2").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: true,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"reject injecting new gate in AdmissionGatedBy annotation": {
 			oldObj: testingstatefulset.MakeStatefulSet("test-sts", "default").
@@ -996,7 +997,7 @@ func TestValidateUpdate(t *testing.T) {
 			wantErr: field.ErrorList{
 				field.Forbidden(admissionGatedByAnnotationsPath, "can only remove gates, not add new ones"),
 			}.ToAggregate(),
-			admissionGatedBy: true,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 		"allow reordering gates in AdmissionGatedBy annotation": {
 			oldObj: testingstatefulset.MakeStatefulSet("test-sts", "default").
@@ -1007,14 +1008,14 @@ func TestValidateUpdate(t *testing.T) {
 				Queue("queue").
 				Annotation(kueueconstants.AdmissionGatedByAnnotation, "example.com/controller2,example.com/controller1").
 				Obj(),
-			wantErr:          nil,
-			admissionGatedBy: true,
+			wantErr:      nil,
+			featureGates: map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 		},
 	}
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
-			features.SetFeatureGateDuringTest(t, features.AdmissionGatedBy, tc.admissionGatedBy)
+			features.SetFeatureGatesDuringTest(t, tc.featureGates)
 			t.Cleanup(jobframework.EnableIntegrationsForTest(t, tc.integrations...))
 
 			client := utiltesting.NewClientBuilder(awv1beta2.AddToScheme, leaderworkersetv1.AddToScheme).

pkg/controller/jobs/trainjob/trainjob_webhook_test.go
@@ -25,6 +25,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/client-go/tools/record"
+	"k8s.io/component-base/featuregate"
 	"k8s.io/utils/ptr"
 	jobsetapi "sigs.k8s.io/jobset/api/jobset/v1alpha2"
 
@@ -205,7 +206,7 @@ func TestDefault(t *testing.T) {
 		manageJobsWithoutQueueName   bool
 		withMultiKueueAdmissionCheck bool
 		withDefaultLocalQueue        bool
-		multiKueueEnabled            bool
+		featureGates                 map[featuregate.Feature]bool
 		wantTrainJob                 *kftrainerapi.TrainJob
 		wantErr                      error
 	}{
@@ -248,7 +249,7 @@ func TestDefault(t *testing.T) {
 				JobSetLabel(controllerconstants.QueueLabel, testLocalQueue.Name).
 				ManagedBy(kueue.MultiKueueControllerName).
 				Obj(),
-			multiKueueEnabled:            true,
+			featureGates:                 map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 			withMultiKueueAdmissionCheck: true,
 		},
 		"should not set managedBy to multiKueue if already specified by the user": {
@@ -258,7 +259,7 @@ func TestDefault(t *testing.T) {
 				JobSetLabel(controllerconstants.QueueLabel, testLocalQueue.Name).
 				ManagedBy("user").
 				Obj(),
-			multiKueueEnabled:            true,
+			featureGates:                 map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 			withMultiKueueAdmissionCheck: true,
 		},
 		"should not set managedBy to multiKueue if the selected clusterQueue doesn't have the corresponding admissionCheck": {
@@ -267,14 +268,14 @@ func TestDefault(t *testing.T) {
 				Suspend(true).
 				JobSetLabel(controllerconstants.QueueLabel, testLocalQueue.Name).
 				Obj(),
-			multiKueueEnabled:            true,
+			featureGates:                 map[featuregate.Feature]bool{features.AdmissionGatedBy: true},
 			withMultiKueueAdmissionCheck: false,
 		},
 	}
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
-			features.SetFeatureGateDuringTest(t, features.MultiKueue, tc.multiKueueEnabled)
+			features.SetFeatureGatesDuringTest(t, tc.featureGates)
 
 			ctx, log := utiltesting.ContextWithLog(t)
 

epam/kubernetes-kueue

pkg/controller/jobs/statefulset/statefulset_webhook_test.go
@@ -28,6 +28,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/component-base/featuregate"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -54,7 +55,7 @@ func TestDefault(t *testing.T) {
 		initObjs                   []client.Object
 		statefulset                *appsv1.StatefulSet
 		manageJobsWithoutQueueName bool
-		localQueueDefaulting       bool
+		featureGates               map[featuregate.Feature]bool
 		defaultLqExist             bool
 		enableIntegrations         []string
 		want                       *appsv1.StatefulSet
@@ -125,7 +126,7 @@ func TestDefault(t *testing.T) {
 				Obj(),
 		},
 		"LocalQueueDefaulting enabled, default lq is created, job doesn't have queue label": {
-			localQueueDefaulting: true,
+			featureGates: map[featuregate.Feature]bool{features.LocalQueueDefaulting: true},
 			defaultLqExist:       true,
 			statefulset:          testingstatefulset.MakeStatefulSet("test-pod", "default").Obj(),
 			want: testingstatefulset.MakeStatefulSet("test-pod", "default").
@@ -135,7 +136,7 @@ func TestDefault(t *testing.T) {
 				Obj(),
 		},
 		"LocalQueueDefaulting enabled, default lq is created, job has queue label": {
-			localQueueDefaulting: true,
+			featureGates: map[featuregate.Feature]bool{features.LocalQueueDefaulting: true},
 			defaultLqExist:       true,
 			statefulset:          testingstatefulset.MakeStatefulSet("test-pod", "").Queue("test-queue").Obj(),
 			want: testingstatefulset.MakeStatefulSet("test-pod", "").
@@ -145,7 +146,7 @@ func TestDefault(t *testing.T) {
 				Obj(),
 		},
 		"LocalQueueDefaulting enabled, default lq isn't created, job doesn't have queue label": {
-			localQueueDefaulting: true,
+			featureGates: map[featuregate.Feature]bool{features.LocalQueueDefaulting: true},
 			defaultLqExist:       false,
 			statefulset:          testingstatefulset.MakeStatefulSet("test-pod", "").Obj(),
 			want:                 testingstatefulset.MakeStatefulSet("test-pod", "").Obj(),
@@ -154,7 +155,7 @@ func TestDefault(t *testing.T) {
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
-			features.SetFeatureGateDuringTest(t, features.LocalQueueDefaulting, tc.localQueueDefaulting)
+			features.SetFeatureGatesDuringTest(t, tc.featureGates)
 			t.Cleanup(jobframework.EnableIntegrationsForTest(t, tc.enableIntegrations...))
 			ctx, _ := utiltesting.ContextWithLog(t)
 

pkg/controller/jobs/trainjob/trainjob_webhook_test.go
@@ -25,6 +25,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/client-go/tools/record"
+	"k8s.io/component-base/featuregate"
 	"k8s.io/utils/ptr"
 	jobsetapi "sigs.k8s.io/jobset/api/jobset/v1alpha2"
 
@@ -205,11 +206,10 @@ func TestDefault(t *testing.T) {
 	testCases := map[string]struct {
 		trainJob                     *kftrainerapi.TrainJob
 		defaultQueue                 *kueue.LocalQueue
-		localQueueDefaultingEnabled  bool
 		manageJobsWithoutQueueName   bool
 		withMultiKueueAdmissionCheck bool
 		withDefaultLocalQueue        bool
-		multiKueueEnabled            bool
+		featureGates                 map[featuregate.Feature]bool
 		wantTrainJob                 *kftrainerapi.TrainJob
 		wantErr                      error
 	}{
@@ -238,13 +238,13 @@ func TestDefault(t *testing.T) {
 				Queue(string(controllerconstants.DefaultLocalQueueName)).
 				JobSetLabel(controllerconstants.QueueLabel, string(controllerconstants.DefaultLocalQueueName)).
 				Obj(),
-			localQueueDefaultingEnabled: true,
+			featureGates: map[featuregate.Feature]bool{features.LocalQueueDefaulting: true},
 			withDefaultLocalQueue:       true,
 		},
 		"should not set the default local queue if doesn't exists": {
 			trainJob:                    testTrainJob.Clone().Obj(),
 			wantTrainJob:                testTrainJob.Clone().Obj(),
-			localQueueDefaultingEnabled: true,
+			featureGates: map[featuregate.Feature]bool{features.LocalQueueDefaulting: true},
 			withDefaultLocalQueue:       false,
 		},
 		"should set managedBy to multiKueue if the user didn't specify any": {
@@ -254,7 +254,7 @@ func TestDefault(t *testing.T) {
 				JobSetLabel(controllerconstants.QueueLabel, testLocalQueue.Name).
 				ManagedBy(kueue.MultiKueueControllerName).
 				Obj(),
-			multiKueueEnabled:            true,
+			featureGates:                 map[featuregate.Feature]bool{features.MultiKueue: true},
 			withMultiKueueAdmissionCheck: true,
 		},
 		"should not set managedBy to multiKueue if already specified by the user": {
@@ -264,7 +264,7 @@ func TestDefault(t *testing.T) {
 				JobSetLabel(controllerconstants.QueueLabel, testLocalQueue.Name).
 				ManagedBy("user").
 				Obj(),
-			multiKueueEnabled:            true,
+			featureGates:                 map[featuregate.Feature]bool{features.MultiKueue: true},
 			withMultiKueueAdmissionCheck: true,
 		},
 		"should not set managedBy to multiKueue if the selected clusterQueue doesn't have the corresponding admissionCheck": {
@@ -273,16 +273,14 @@ func TestDefault(t *testing.T) {
 				Suspend(true).
 				JobSetLabel(controllerconstants.QueueLabel, testLocalQueue.Name).
 				Obj(),
-			multiKueueEnabled:            true,
+			featureGates:                 map[featuregate.Feature]bool{features.MultiKueue: true},
 			withMultiKueueAdmissionCheck: false,
 		},
 	}
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
-			features.SetFeatureGateDuringTest(t, features.MultiKueue, tc.multiKueueEnabled)
-			features.SetFeatureGateDuringTest(t, features.LocalQueueDefaulting, tc.localQueueDefaultingEnabled)
-
+			features.SetFeatureGatesDuringTest(t, tc.featureGates)
 			ctx, log := utiltesting.ContextWithLog(t)
 
 			kClient := utiltesting.NewClientBuilder().WithObjects(testNamespace.Obj()).Build()